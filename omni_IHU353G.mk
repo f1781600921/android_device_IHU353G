35041a88a070a26731c703cbfd42bd7e9ea7c197
f1781600921 <49328623+f1781600921@users.noreply.github.com>
2024-06-26 11:12:08 +0800

    Update omni_IHU353G.mk

--- omni_IHU353G.mk
+++ omni_IHU353G.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := IHU353G
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -27,3 +30,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="IHU353G-user 9 PQ2A.190405.003 75 dev-keys"
 
 BUILD_FINGERPRINT := ecarx/IHU353G/IHU353G:9/PQ2A.190405.003/75:user/dev-keys
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
